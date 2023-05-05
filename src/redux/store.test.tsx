e2d704f73cf6c321fbca65730601da391417c5ca
Dmytro Tymchenko <timchenkod88@gmail.com>
2023-05-05 20:03:39 +0300

    added store test

--- src/redux/store.test.tsx
+++ src/redux/store.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import mainReducer, { setInputValue } from "./slices/mainSlice";
+import mainReducer, { setInputValue, setHelper } from "./slices/mainSlice";
 import App from "../App";
 
 const renderWithRedux = (
@@ -25,5 +25,18 @@ describe("setInputValue", () => {
     const testInputValue = "Test Input Value";
     store.dispatch(setInputValue(testInputValue));
     expect(store.getState().main.inputValue).toEqual(testInputValue);
+  });
+});
+
+describe("setHelper", () => {
+  test("toggles helper flag correctly", () => {
+    const { store } = renderWithRedux(<App />, {});
+    expect(store.getState().main.helper).toBeFalsy();
+
+    store.dispatch(setHelper());
+    expect(store.getState().main.helper).toBeTruthy();
+
+    store.dispatch(setHelper());
+    expect(store.getState().main.helper).toBeFalsy();
   });
 });
